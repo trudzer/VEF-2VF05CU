87205a22465a2a1b576d4f2fd1b4e57b9e110582
trudzer <trudzeredits@gmail.com>
2019-01-16 09:42:27 +0000

    Update app.py

--- verkefni2/app.py
+++ verkefni2/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from datetime import datetime
 import re
 
 app = Flask(__name__)
@@ -6,11 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Halló Heimur"
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/two">sida 2</a>
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/home/three">sida 3</a>
+    
 
 @app.route("/two")
 def two():
     return "Hello, two!"
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/">sida 1</a>
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/home/three">sida 3</a>
 
 @app.route("/home/three")
 def homethree():
     return "Hello, three!"
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/">sida 1</a>
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/two">sida 2</a>
