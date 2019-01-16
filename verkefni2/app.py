d4a574daa048603f871c8014dd750cbf96dfa5cf
trudzer <trudzeredits@gmail.com>
2019-01-16 09:48:13 +0000

    Update app.py

--- verkefni2/app.py
+++ verkefni2/app.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """Halló Heimur
-    <a href="https://vefth2vf0-verkefni2.herokuapp.com/two">sida 2</a>
-    <a href="https://vefth2vf0-verkefni2.herokuapp.com/home/three">sida 3</a> """
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/two">\nsida 2</a>
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/home/three">\nsida 3</a> """
     
 
 @app.route("/two")
 def two():
     return """Hello, two!
-    <a href="https://vefth2vf0-verkefni2.herokuapp.com/">sida 1</a>
-    <a href="https://vefth2vf0-verkefni2.herokuapp.com/home/three">sida 3</a> """
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/">\nsida 1</a>
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/home/three">\nsida 3</a> """
 
 @app.route("/home/three")
 def homethree():
     return """Hello, three!
-    <a href="https://vefth2vf0-verkefni2.herokuapp.com/">sida 1</a>
-    <a href="https://vefth2vf0-verkefni2.herokuapp.com/two">sida 2</a> """
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/">\nsida 1</a>
+    <a href="https://vefth2vf0-verkefni2.herokuapp.com/two">\nsida 2</a> """
